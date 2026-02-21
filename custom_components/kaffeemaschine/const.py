34d94377e66630f57445a0300f87c8034239ff56
andreasschiffmann86-prog <andreas.schiffmann86@gmail.com>
2026-02-21 22:06:25 +0100

    feat: Add Kaffeemaschine MQTT Timeline Integration for Home Assistant

---
+++ custom_components/kaffeemaschine/const.py
@@ -0,0 +1,18 @@
+DOMAIN = "kaffeemaschine"
+CONF_TOPIC = "mqtt_topic"
+CONF_QOS = "qos"
+DEFAULT_TOPIC = "kaffeemaschine/getraenk"
+DEFAULT_QOS = 0
+STORAGE_KEY = "kaffeemaschine_timeline"
+STORAGE_VERSION = 1
+MAX_ENTRIES = 1000
+
+GETRAENKE_ICONS = {
+    "Espresso": "☕",
+    "Kaffee": "☕",
+    "Cappuccino": "🍵",
+    "Latte Macchiato": "🥛",
+    "Americano": "☕",
+    "Heißwasser": "💧",
+    "Dampf": "💨",
+}
